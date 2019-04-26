b38a98112457e61effb9bce5fe69643fd7abed5c
Philip Karlsson <philip.karlsson@husqvarnagroup.com>
2019-04-26 20:17:28 +0200

    Added TMUX resizing

--- keyboards/hhkb/keymaps/philip/keymap.c
+++ keyboards/hhkb/keymaps/philip/keymap.c
@@ -19,12 +19,52 @@ enum custom_keycodes {
   GDB_C,
   GDB_SI,
   GDB_NI,
-  GDB_FIN
+  GDB_FIN,
+  TMUX_RESIZE_PANE_H,
+  TMUX_RESIZE_PANE_J,
+  TMUX_RESIZE_PANE_K,
+  TMUX_RESIZE_PANE_L
 };
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case TMUX_RESIZE_PANE_H:
+      if (record->event.pressed) {
+        // when keycode P_PARENT is pressed
+        SEND_STRING(SS_LCTRL("a")"H");
+      } else {
+        // when keycode P_PARENT is released
+      }
+      break;
+
+    case TMUX_RESIZE_PANE_J:
+      if (record->event.pressed) {
+        // when keycode P_PARENT is pressed
+        SEND_STRING(SS_LCTRL("a")"J");
+      } else {
+        // when keycode P_PARENT is released
+      }
+      break;
+
+    case TMUX_RESIZE_PANE_K:
+      if (record->event.pressed) {
+        // when keycode P_PARENT is pressed
+        SEND_STRING(SS_LCTRL("a")"K");
+      } else {
+        // when keycode P_PARENT is released
+      }
+      break;
+
+    case TMUX_RESIZE_PANE_L:
+      if (record->event.pressed) {
+        // when keycode P_PARENT is pressed
+        SEND_STRING(SS_LCTRL("a")"L");
+      } else {
+        // when keycode P_PARENT is released
+      }
+      break;
+
     case P_PARENT:
       if (record->event.pressed) {
         // when keycode P_PARENT is pressed
@@ -172,7 +212,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [GDB] = LAYOUT(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, GDB_C, KC_6, KC_7, KC_8, KC_9, GDB_NI, GDB_SI, GDB_FIN, KC_BSLS, KC_GRV,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_BSPC, KC_ENT, KC_TRNS, KC_TAB, KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, TMUX_RESIZE_PANE_H, TMUX_RESIZE_PANE_J, TMUX_RESIZE_PANE_K, TMUX_RESIZE_PANE_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, P_PARENT, KC_SLSH, KC_RSFT, MO(FN),
         KC_LGUI, KC_LALT, /*        */ (KC_SPC), KC_RGUI, KC_RALT)
 };
