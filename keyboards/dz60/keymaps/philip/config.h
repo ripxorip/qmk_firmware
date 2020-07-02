8fcc63d3654e032b535ae3df00070081955150a7
Philip Karlsson <philip.karlsson@husqvarnagroup.com>
2020-07-02 15:25:39 +0200

    More mouse again

--- keyboards/dz60/keymaps/philip/config.h
+++ keyboards/dz60/keymaps/philip/config.h
@@ -1,5 +1,5 @@
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    120
-#define MOUSEKEY_MAX_SPEED      5
+#define MOUSEKEY_TIME_TO_MAX    60
+#define MOUSEKEY_MAX_SPEED      3
 #define MOUSEKEY_WHEEL_DELAY 0
