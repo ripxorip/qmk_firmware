52b6841da8a891bd80f99bbb94703631ec7fabca
Philip Karlsson <philip.karlsson@husqvarnagroup.com>
2020-12-09 17:34:32 +0100

    First working version on target

--- keyboards/handwired/dactyl_manuform/5x6/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/config.h
@@ -30,14 +30,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 6
 
 // wiring of each half
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
+
+#define MATRIX_ROW_PINS_RIGHT { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6 }
 
 #define DIODE_DIRECTION COL2ROW
 
 // WS2812 RGB LED strip input and number of LEDs
 #define RGB_DI_PIN D3
-#define RGBLED_NUM 12
+#define RGBLED_NUM 16
+
+#define RGBLED_SPLIT { 8, 8 }
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
