a24adfa95bff33b20b7eab8c0fbabd5cf1468c7a
Philip Karlsson <philip.karlsson@husqvarnagroup.com>
2020-12-11 16:59:32 +0100

    Work in progress before evening walk

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/ripxorip/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/ripxorip/keymap.c
@@ -3,6 +3,10 @@
 #include QMK_KEYBOARD_H
 #include "ripxorip.h"
 
+/* FIXME rewrite so that I have a number of function
+ its too much of a waste to have separater layers for
+ categories */
+
 enum layers
 {
     BASE = 0,
@@ -18,17 +22,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
      KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                      KC_NO ,KC_RBRC,                                                       KC_LBRC, KC_RBRC,
-                                      KC_LALT ,KC_SPC,                         MO(SPEC_CHAR), MO(MOTION),
-                                      LCTL(KC_A),KC_LALT,                            KC_LCTL,  KC_LGUI,
+                      KC_LSFT ,KC_RBRC,                                                       KC_LBRC, KC_RBRC,
+                                      KC_BSPC ,KC_SPC,                         KC_LCTL, MO(MOTION),
+                                      LCTL(KC_A),KC_LALT,                            KC_LALT,  KC_LGUI,
                                       KC_E, MO(GUI),                              KC_E, KC_F
    ),
 
   /* This mode shall have support for removing words quickly using old fashion editing commands e.g. ctrl backspace in a VIM agnostic style */
   [MOTION] = RIPXORIP_5x6(
      KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                    KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  ,KC_F11,
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         LCTL(KC_BSPC)  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_LEFT  , KC_DOWN  , KC_UP  , KC_RIGHT  ,KC_SCLN,KC_QUOT,
+     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         LCTL(KC_BSPC)  , KC_U  , KC_I  , KC_O  , KC_P  ,UC(L'å'),
+     KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_LEFT  , KC_DOWN  , KC_UP  , KC_RIGHT  ,UC(L'ö'),UC(L'ä'),
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                       KC_LBRC,KC_RBRC,                                                       KC_LBRC, KC_RBRC,
                                       KC_LALT ,KC_SPC,                         KC_SPC, KC_SPC,
